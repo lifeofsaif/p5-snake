f849a47a21c5a4268a27b9171a5c995d2a112eb0
Abdur Rahman <SaifHomeFolder@Abdurs-MacBook-Air.local>
2017-05-15 15:23:57 -0700

    added death, colors, regeneration

--- sketch.js
+++ sketch.js
@@ -1,20 +1,88 @@
 var s
 var scl = 20
-
+var frame = 0
+var originalframecount = 10
+var framecount = originalframecount
 
 function setup() {
     createCanvas(600, 600)
-    s = new snake(scl, scl, 0, 0, 'green')
-    frameRate(20)
+    s = new snake(300 - scl, 300 - scl, 0, 0, 'green')
+    frameRate(framecount)
+
+}
+
+function getColor(length) {
+    index = length % 11
+    switch (index) {
+        case 0:
+            return '#00E517'
+            break;
+        case 1:
+            return '#00E5A3'
+            break;
+        case 2:
+            return '#0099E5'
+            break;
+        case 3:
+            return '#000Ce5'
+            break;
+        case 4:
+            return '#8000E5'
+            break;
+        case 5:
+            return '#E500BC'
+            break;
+        case 6:
+            return '#E50036'
+            break;
+        case 7:
+            return '#E54F00'
+            break;
+        case 8:
+            return '#E5D400'
+            break;
+        case 9:
+            return '#6FE500'
+            break;
+        case 10:
+            return '#00E516'
+            break;
+
+    }
+    return 'black'
 }
 
 function draw() {
-    background(167, 183, 165)
-    s.update()
+
+    background("#fffd91")
+    if (s)
+        s.update()
+
+    stroke(getColor(frame))
+    line(599, 0, 599, 599);
+    line(0, 0, 0, 600)
+    line(0, 0, 600, 0)
+    line(0, 599, 599, 599)
+
+    frame++
+}
+
+function mousePressed() {
+    if (!s) {
+        s = new snake(300 - scl, 300 - scl, 0, 0, 'green')
+
+    }
+}
+
+function killSnake() {
+    s = 0
+    framecount = originalframecount
+    frameRate(framecount)
 }
 
 function keyPressed() {
-    s.dir(keyCode)
+    if (s)
+        s.dir(keyCode)
 }
 
 function apple() {
--- snake.js
+++ snake.js
@@ -2,76 +2,84 @@ function snake(x, y, xspeed, yspeed, color) {
     this.location = createVector(x, y)
     this.speed = createVector(xspeed, yspeed)
     this.score = 0;
-    this.a = new apple(2 * scl, 2 * scl)
+    this.a = this.a = new apple(Math.floor(random(600 / scl)) * scl, Math.floor(random(600 / scl)) * scl)
     this.tLength = 0
     this.tail = []
     this.update = function () {
+
+
+
+
         this.location.x += this.speed.x
         this.location.y += this.speed.y
         this.location.x = constrain(this.location.x, 0, 600 - scl - 1)
         this.location.y = constrain(this.location.y, 0, 600 - scl - 1)
-        if (this.getDistance() < 5) {
-            this.createNewApple()
-            this.extendTail()
-        }
+
         for (var i = this.tail.length - 1; i > -1; i--) {
+            if (dist(this.tail[i].location.x, this.tail[i].location.y, this.location.x, this.location.y) < 1)
+                killSnake()
+            else if (dist(this.tail[i].location.x, this.tail[i].location.y, this.a.location.x, this.a.location.y) < 1)
+                this.createNewApple()
+
             this.tail[i].update()
+
             if (i == 0) {
                 this.tail[i].updateLocation(this.location.x, this.location.y)
+            } else {
+                this.tail[i].updateLocation(this.tail[i - 1].location.x, this.tail[i - 1].location.y)
             }
-            else {
-                this.tail[i].updateLocation(this.tail[i-1].location.x, this.tail[i-1].location.y)
-            }
-            
+        }
+        if (this.getDistance() < 5) {
+            this.createNewApple()
         }
         fill(color)
         rect(this.location.x, this.location.y, scl, scl)
         this.a.show()
+
     }
     this.extendTail = function () {
-        console.log('extending tail')
-        this.tail.push(new tailUnit(this.location.x, this.location.y))
+        this.tail.push(new tailUnit(this.location.x, this.location.y, getColor(this.tail.length)))
     }
 
-    function tailUnit(x, y) {
+    function tailUnit(x, y, color) {
         this.location = createVector(x, y)
         this.update = function () {
-            fill('yellow')
+            fill(color)
             rect(this.location.x, this.location.y, scl, scl)
         }
-        this.updateLocation = function (x,y) {
+        this.updateLocation = function (x, y) {
             this.location.x = x
             this.location.y = y
         }
         this.getLocation = function () {
             return {
-                x: this.location.x, 
+                x: this.location.x,
                 y: this.location.y
             }
         }
     }
     this.createNewApple = function () {
+        ++framecount
+        frameRate(++framecount)
         this.a = new apple(Math.floor(random(600 / scl)) * scl, Math.floor(random(600 / scl)) * scl)
+        this.extendTail()
     }
     this.dir = function (keyCode) {
-            if (keyCode === DOWN_ARROW) {
-                this.speed.x = 0
-                this.speed.y = scl
-            }
-            else if (keyCode === UP_ARROW) {
-                this.speed.x = 0
-                this.speed.y = -scl
-            }
-            else if (keyCode === LEFT_ARROW) {
-                this.speed.x = -scl
-                this.speed.y = 0
-            }
-            else if (keyCode === RIGHT_ARROW) {
-                this.speed.x = scl
-                this.speed.y = 0
-            }
+        if (keyCode === DOWN_ARROW) {
+            this.speed.x = 0
+            this.speed.y = scl
+        } else if (keyCode === UP_ARROW) {
+            this.speed.x = 0
+            this.speed.y = -scl
+        } else if (keyCode === LEFT_ARROW) {
+            this.speed.x = -scl
+            this.speed.y = 0
+        } else if (keyCode === RIGHT_ARROW) {
+            this.speed.x = scl
+            this.speed.y = 0
         }
-        //apple construction 
+    }
+    //apple construction 
     function apple(x, y) {
         //this.location = createVector(Math.floor(random(width / scl)) * scl, Math.floor(random(height / scl)) * scl)
         this.location = createVector(x, y)
@@ -83,4 +91,5 @@ function snake(x, y, xspeed, yspeed, color) {
     this.getDistance = function () {
         return dist(this.location.x, this.location.y, this.a.location.x, this.a.location.y)
     }
+
 }
