6b271170f9113297973e3c960033482a6703aa74
Abdur Rahman <SaifHomeFolder@Abdurs-MacBook-Air.local>
2017-05-15 11:35:49 -0700

    added express/ heroku configurations

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+var express = require('express')
+var app = express();
+var port = process.env.PORT || 8080;
+app.use(express.static(__dirname))
+app.listen(port, function () {
+    console.log('Our app is running on http://localhost:' + port);
+});
