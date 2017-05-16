aa43e65273ee9a846e4261065715bb5071d6a88b
Abdur Rahman <SaifHomeFolder@Abdurs-MacBook-Air.local>
2017-05-15 23:15:31 -0700

    adding error throw for file write for heroku

--- app.js
+++ app.js
@@ -4,24 +4,20 @@ var app = express();
 var port = process.env.PORT || 8080;
 var bodyParser = require('body-parser');
 var fs = require('fs')
-
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
-
-
-
 app.listen(port, function () {
     console.log('Our app is running on http://localhost:' + port);
 });
-
-app.get('/api/trophies',function(req,res){
+app.get('/api/trophies', function (req, res) {
     res.json(require('./trophy.json'))
 })
-
-app.post('/', function(req, res){
-    fs.writeFile("./trophy.json", JSON.stringify({score: ""+req.body.score+"", message:req.body.message}))
+app.post('/', function (req, res) {
+    fs.writeFile("./trophy.json", JSON.stringify({
+        score: "" + req.body.score + ""
+        , message: req.body.message
+    }), function (err) {
+        if (err) throw err;
+    })
     res.send()
 })
-
-
-
